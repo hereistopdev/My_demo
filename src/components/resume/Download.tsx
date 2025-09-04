014853c358609d8a86468fdc6c3a56deac725a34
hereistopdev <hereis.topdev@gmail.com>
2025-09-04 10:49:00 -0400

    updated resume engish

--- src/components/resume/Download.tsx
+++ src/components/resume/Download.tsx
@@ -2,7 +2,7 @@ import { Button, Card, IconButton, Stack, Tooltip, Typography } from "@mui/joy";
 import details from "@/assets/Details";
 import { useMobileMode } from "@/components/Responsive";
 import useOverlayQueryParam from "@/navigation/useOverlayQueryParam";
-import pdf from "@/assets/pdf/my-resume.pdf";
+import pdf from "@/assets/pdf/dustin.pdf";
 import { LuDownload,  LuFileText, LuGlasses } from "react-icons/lu";
 
 /**
